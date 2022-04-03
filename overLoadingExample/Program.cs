a7cdb5ddcef8bf5ebc11278d88ab0ef2617a0362
judeanaman <judeanaman@gmail.com>
2022-04-03 23:22:48 +0000

    overload

---
+++ overLoadingExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overLoadingExample
+{
+    class muc
+    {
+        // adding three integer values.
+
+    public int Add(int a, int b, int c)
+    {
+        int sum = a + b + c;
+
+        return sum;
+    }
+ 
+    // adding three double values.
+    public double Multiplication(double a, double b, double c)
+    {
+        double sum = a * b * c;
+
+        return sum;
+    }
+        static void Main(string[] args)
+        {
+            // Creating Object
+            muc ob = new muc();
+
+            int sum2 = ob.Add(1, 2, 3);
+
+            Console.WriteLine("sum of the three " + "integer value : " + sum2);
+
+            double sum3 = ob.Multiplication(4.0, 2.0, 5.0);
+
+            Console.WriteLine("sum of the three "  + "double value : " + sum3);
+
+        }
+    }
+}
